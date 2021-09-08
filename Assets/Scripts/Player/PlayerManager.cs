74cab6078d079d31312609cc4fc49d6fb3d8b737
Prabhav Bhatnagar <prabhav2b@gmail.com>
2021-09-08 20:46:55 +0300

    DoubleTapTest

--- Assets/Scripts/Player/PlayerManager.cs
+++ Assets/Scripts/Player/PlayerManager.cs
@@ -93,6 +93,12 @@ public class PlayerManager : SingleInstance<PlayerManager>
     {
         CurrentControlScheme = schemeName.Equals("Gamepad") ? ControlScheme.Gamepad : ControlScheme.KeyboardAndMouse;
     }
+    
+    private void OnDoubleClickTest()
+    {
+        Debug.Log("Double Click!");
+    
+    }
 
     private void OnMainMenu(InputValue input)
     {
