241c2557fbef14f5dbeaaf4d1925efd814edc4f9
Toni Sariola <37574272+tonisariola@users.noreply.github.com>
2021-09-11 15:59:07 +0300

    Add map icon

--- Assets/Scripts/Map/ObjectToggler.cs
+++ Assets/Scripts/Map/ObjectToggler.cs
@@ -1,9 +1,36 @@
 using UnityEngine;
+using UnityEngine.Events;
 
 [DisallowMultipleComponent]
 public class ObjectToggler : MonoBehaviour
 {
-    public void Toggle() => gameObject.SetActive(!gameObject.activeSelf);
-    public void Open() => gameObject.SetActive(true);
-    public void Close() => gameObject.SetActive(false);
+    public UnityEvent OnToggle = new UnityEvent();
+    public UnityEvent OnOpen = new UnityEvent();
+    public UnityEvent OnClose = new UnityEvent();
+
+    public void Toggle()
+    {
+        gameObject.SetActive(!gameObject.activeSelf);
+        if (gameObject.activeSelf)
+        {
+            OnOpen.Invoke();
+        }
+        else
+        {
+            OnClose.Invoke();
+        }
+        OnToggle.Invoke();
+    }
+
+    public void Open()
+    {
+        gameObject.SetActive(true);
+        OnOpen.Invoke();
+    }
+
+    public void Close()
+    {
+        gameObject.SetActive(false);
+        OnClose.Invoke();
+    }
 }
