12c35ceb6628a723ff86108c3269a177c3bdce6a
Prabhav Bhatnagar <prabhav2b@gmail.com>
2021-09-12 19:16:21 +0300

    Game Start complete

--- Assets/Scripts/One Off/DummyTrainAnimation.cs
+++ Assets/Scripts/One Off/DummyTrainAnimation.cs
@@ -1,20 +1,27 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using DG.Tweening;
+using UnityEngine.InputSystem;
 
 public class DummyTrainAnimation : MonoBehaviour
 {
+    [SerializeField] private GameObject menuVisuals;
+    
     [SerializeField] private float positionA;
     [SerializeField] private float positionB;
 
     private float _pos;
     
     private RectTransform _trainTransform;
+
+    bool _scriptedTrainEvent;
     
     void Start()
     {
+        FindObjectOfType<PlayerManager>().GetComponent<PlayerInput>().enabled = false;
         _trainTransform = GetComponent<RectTransform>();
 
         _trainTransform.anchoredPosition = new Vector2(positionA, _trainTransform.anchoredPosition.y);
@@ -55,5 +62,26 @@ public class DummyTrainAnimation : MonoBehaviour
         _trainTransform.DOAnchorPosX(_pos, 7f).OnComplete(DummyWait);
         _pos = tempPos;
 
+    }
+
+    private void Update()
+    {
+        if(!_scriptedTrainEvent)
+            return;
+
+        if (_trainTransform.anchoredPosition.x < -14000f)
+        {
+            menuVisuals.SetActive(false);
+        }
+
+    }
+
+    public void ScriptedTrainEvent()
+    {
+        DOTween.Kill(_trainTransform);
+         StopAllCoroutines();
+         _trainTransform.anchoredPosition = new Vector2(positionA, _trainTransform.anchoredPosition.y);
+        _trainTransform.DOAnchorPosX(positionB, 10f).OnComplete(()=> transform.parent.gameObject.SetActive(false));
+        _scriptedTrainEvent = true;
     }
 }
