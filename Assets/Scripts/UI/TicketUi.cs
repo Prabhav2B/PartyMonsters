4d885aa03363a27ff228c36d7150227ae945369a
Prabhav Bhatnagar <prabhav2b@gmail.com>
2021-09-12 04:21:09 +0300

    ticket ui for the map

---
+++ Assets/Scripts/UI/TicketUi.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TicketUi : MonoBehaviour
+{
+    [SerializeField] Sprite missingTicket;
+    [SerializeField] Sprite blueTicket;
+    [SerializeField] Sprite pinkTicket;
+    [SerializeField] Sprite greenTicket;
+    [SerializeField] Sprite yellowTicket;
+    [SerializeField] Sprite purpleTicket;
+    [SerializeField] Sprite whiteTicket;
+
+    
+    private SpriteRenderer[] _ticketSprites;
+    
+    void Awake()
+    {
+        _ticketSprites = GetComponentsInChildren<SpriteRenderer>();
+    }
+
+    public void UpdateTicketSprites()
+    {
+        _ticketSprites[0].sprite = PlayerInventory.ContainsItemOfType(ItemType.TicketBlue) ? blueTicket : missingTicket;
+        _ticketSprites[1].sprite = PlayerInventory.ContainsItemOfType(ItemType.TicketPink) ? pinkTicket : missingTicket;
+        _ticketSprites[2].sprite = PlayerInventory.ContainsItemOfType(ItemType.TicketGreen) ? greenTicket : missingTicket;
+        _ticketSprites[3].sprite = PlayerInventory.ContainsItemOfType(ItemType.TicketYellow) ? yellowTicket : missingTicket;
+        _ticketSprites[4].sprite = PlayerInventory.ContainsItemOfType(ItemType.TicketPurple) ? purpleTicket : missingTicket;
+
+    }
+}
